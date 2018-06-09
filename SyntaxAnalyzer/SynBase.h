06abb1ddf7eb5a085849a0f4312797e8314c8fad
Subangkar [Gitkraken-HP-Ubuntu18] <subangkar.karmaker@gmail.com>
2018-06-09 13:16:38 +0600

    CodeParser moved to Utils.h

--- SyntaxAnalyzer/SynBase.h
+++ SyntaxAnalyzer/SynBase.h
@@ -118,130 +118,6 @@ string popVal(NONTERMINAL_TYPE nonterminal) {
 
 
 
-string indent(string fileName)//Autoindent a file
-{
-
-	int pos = 0, count =0, j=0;
-	string ch;
-	stringstream file(fileName);
-//	ifstream file(fileName);
-//	while(file.good())
-//	{
-//		pos++;//return value at end of code, number of lines
-//		getline(file,ch);
-//	}
-	pos = StringUtils::occCount(fileName,"\n");
-	vector<string> hold(pos+1);//holds the actual content of the line.
-	vector<int> bracketcount(pos+1);//keeps track of number of nested brackets
-
-//	file.clear();
-//	file.seekg(0);
-	while(getline(file,hold[count]))
-	{
-		count++;
-	}
-	int endline = count;
-	int p=0;
-	while(p<=endline)
-	{
-		bracketcount[p]=0;
-		p++;
-	}
-	count = 0;
-	int len=0,i = 0, current=0;
-	while(count<endline)
-	{
-		len = static_cast<int>(hold[count].length());
-		i=0;
-		while(i<=len)
-		{
-			if(hold[count][i] == '{')
-			{
-				current=count;
-				while(current <endline)
-				{
-					current++;
-					bracketcount[current]++;
-				}
-			}
-			if(hold[count][i] == '}')
-			{
-				current=count;
-				while(current <endline)
-				{
-					bracketcount[current]--;
-					current++;
-				}
-			}
-			i++;
-		}
-		count++;
-	}
-//	file.close();
-
-
-
-//	ofstream file2("temp.txt");// consider this a safety net, change to fileName if you trust the code.
-	stringstream file2;
-	// I'm probably just being overly cautious.
-	vector<string> indents(static_cast<unsigned long>(pos + 1));
-	p=0;
-	while(p<endline)
-	{
-		count=0;
-		while(hold[p][count]=='\t')//check if there were already tabs in the file
-		{
-			count++;
-		}
-		while(bracketcount[p]>0)//tells indents[p] how many indents should be in a line
-		{
-			bracketcount[p]--;
-			indents[p] +="\t";
-		}
-		if(count==0)//if the line wasn't previously indented in the code
-		{
-			file2<<indents[p]<<hold[p]<<endl;
-		}
-		else if(count>0)//If there were already indents in the code then set them to the right amount
-		{
-			file2<<indents[p];
-			len = static_cast<int>(hold[p].length());
-			i=0;
-			while(i<len-count)
-			{
-				file2<<hold[p][i+count];
-				i++;
-			}
-			file2<<endl;
-		}
-		p++;
-	}
-//	file2.close();
-//	return pos;
-	return file2.str();
-}
-
-
-string formatCCode(const string &code)
-{
-	string formattedCode = code;
-
-	StringUtils::replaceAll(formattedCode,";",";\n");
-	StringUtils::replaceAll(formattedCode,";\n\n",";\n");
-
-	StringUtils::replaceAll(formattedCode,"\n ","\n");
-
-	StringUtils::replaceAll(formattedCode,"{","\n{\n");
-
-	StringUtils::replaceAll(formattedCode,"}","\n}");
-	StringUtils::replaceAll(formattedCode,"\n\n}","\n}");
-
-	return indent(formattedCode);
-}
-
-
-
-
 
 void printRule(const string &rule) {
 	logFile << "At line no: " << line_count << " " << rule << endl << endl;
@@ -249,7 +125,7 @@ void printRule(const string &rule) {
 
 // after pushing value
 void printCode(NONTERMINAL_TYPE ruleNonterminal){
-	logFile << formatCCode(nonTerminalBuffer.getValue(ruleNonterminal)) << endl << endl;
+	logFile << CodeParser::formatCCode(nonTerminalBuffer.getValue(ruleNonterminal)) << endl << endl;
 }
 
 
--- SyntaxAnalyzer/Utils.h
+++ SyntaxAnalyzer/Utils.h
@@ -43,16 +43,14 @@ public:
 		return parse(string(str));
 	}
 
-	static char parseChar(const string str)
-	{
+	static char parseChar(const string str) {
 		return static_cast<char>(str.length() > 2 ? '\0' : parse(str)[0]);
 	}
 
-	static string toUpperCase(string str)
-	{
+	static string toUpperCase(string str) {
 		std::locale loc;
 		for (char &i : str)
-			i = toupper(i,loc);
+			i = toupper(i, loc);
 		return str;
 	}
 };
@@ -99,5 +97,104 @@ public:
 	}
 };
 
+
+class CodeParser {
+public:
+
+	static string indent(const string &fileName)//Autoindent a file
+	{
+
+		int pos = 0, count = 0, j = 0;
+		string ch;
+		stringstream file(fileName);
+		pos = StringUtils::occCount(fileName, "\n");
+		vector<string> hold(pos + 1);//holds the actual content of the line.
+		vector<int> bracketcount(pos + 1);//keeps track of number of nested brackets
+
+		while (getline(file, hold[count])) {
+			count++;
+		}
+		int endline = count;
+		int p = 0;
+		while (p <= endline) {
+			bracketcount[p] = 0;
+			p++;
+		}
+		count = 0;
+		int len = 0, i = 0, current = 0;
+		while (count < endline) {
+			len = static_cast<int>(hold[count].length());
+			i = 0;
+			while (i <= len) {
+				if (hold[count][i] == '{') {
+					current = count;
+					while (current < endline) {
+						current++;
+						bracketcount[current]++;
+					}
+				}
+				if (hold[count][i] == '}') {
+					current = count;
+					while (current < endline) {
+						bracketcount[current]--;
+						current++;
+					}
+				}
+				i++;
+			}
+			count++;
+		}
+
+
+		stringstream file2;
+		// I'm probably just being overly cautious.
+		vector<string> indents(static_cast<unsigned long>(pos + 1));
+		p = 0;
+		while (p < endline) {
+			count = 0;
+			while (hold[p][count] == '\t')//check if there were already tabs in the file
+			{
+				count++;
+			}
+			while (bracketcount[p] > 0)//tells indents[p] how many indents should be in a line
+			{
+				bracketcount[p]--;
+				indents[p] += "\t";
+			}
+			if (count == 0)//if the line wasn't previously indented in the code
+			{
+				file2 << indents[p] << hold[p] << endl;
+			} else if (count > 0)//If there were already indents in the code then set them to the right amount
+			{
+				file2 << indents[p];
+				len = static_cast<int>(hold[p].length());
+				i = 0;
+				while (i < len - count) {
+					file2 << hold[p][i + count];
+					i++;
+				}
+				file2 << endl;
+			}
+			p++;
+		}
+		return file2.str();
+	}
+
+	static string formatCCode(const string &code) {
+		string formattedCode = code;
+
+		StringUtils::replaceAll(formattedCode, ";", ";\n");
+		StringUtils::replaceAll(formattedCode, ";\n\n", ";\n");
+
+		StringUtils::replaceAll(formattedCode, "\n ", "\n");
+
+		StringUtils::replaceAll(formattedCode, "{", "\n{\n");
+
+		StringUtils::replaceAll(formattedCode, "}", "\n}");
+		StringUtils::replaceAll(formattedCode, "\n\n}", "\n}");
+
+		return indent(formattedCode);
+	}
+};
 
 #endif //LEXICALANALYZER_UTILS_H
